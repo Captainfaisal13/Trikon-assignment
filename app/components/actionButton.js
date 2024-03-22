46d93357feb3d747b09c6891b57901309de002dc
captainfaisal13 <shaikhfaisal3412@gmail.com>
2024-03-22 23:25:09 +0530

    Section1 -Responsiveness [DONE]

--- app/components/actionButton.js
+++ app/components/actionButton.js
@@ -9,7 +9,7 @@ const ActionButton = ({ righteous, content, width, height }) => {
         {content.toUpperCase()}
       </button> */}
       <button
-        className={`bg-[#BEF56E] px-16 py-3 rounded-tl-[10px] rounded-br-[10px] ${righteous.className} text-[16px] border border-[#B5B5B5] text-[#141414]`}
+        className={`bg-[#BEF56E] px-10 py-2 lg:px-12 lg:py-2 xl:px-16 xl:py-3 rounded-tl-[10px] rounded-br-[10px] ${righteous.className} lg:text-[12px] xl:text-[16px] border border-[#B5B5B5] text-[#141414]`}
       >
         {content.toUpperCase()}
       </button>
--- app/components/carouselPage.js
+++ app/components/carouselPage.js
@@ -7,16 +7,16 @@ import ActionButton from "./actionButton";
 const CarouselPage = ({ galactus, montserrat, righteous }) => {
   const [currentGameIndex, setCurrentGameIndex] = useState(0);
   return (
-    <div className="w-full flex justify-evenly pb-28">
-      <div className="md:px-[1.8rem]  lg:px-[2rem] xl:px-[5rem] pt-20 basis-2/5">
-        <div className="">
+    <div className="w-full flex flex-col gap-8 lg:gap-0 lg:flex-row justify-evenly pb-10 lg:pb-28">
+      <div className="px-4 lg:px-[1.5rem] xl:px-[5rem] pt-10 lg:pt-12 xl:pt-20 basis-2/5">
+        <div className="text-center lg:text-left">
           <h3
-            className={`text-[#BEF56E] ${galactus.className} md:text-[12px] lg:text-[16px] leading-4 mb-8`}
+            className={`text-[#bccf9e] ${galactus.className} text-[14px] xl:text-[16px] xl:leading-4 mb-3 lg:mb-4 xl:mb-8`}
           >
             EXPLORE A NEW DIMENSION
           </h3>
           <h1
-            className={`text-white md:text-[32px]  text-[40px] ${montserrat.className} leading-[52px] mb-10`}
+            className={`text-white text-[24px] lg:text-[32px] xl:text-[40px] ${montserrat.className} xl:leading-[52px] mb-5 lg:mb-5 xl:mb-10`}
           >
             Ready to unlock your potentials in a world of fun?
           </h1>
@@ -33,13 +33,13 @@ const CarouselPage = ({ galactus, montserrat, righteous }) => {
           </button> */}
         </div>
       </div>
-      <div className="basis-3/5 flex flex-col gap-8">
-        <div className="flex gap-4 md:max-w-[600px] lg:max-w-[920px] overflow-x-hidden">
+      <div className="pl-4 basis-3/5 flex flex-col gap-8">
+        <div className="flex gap-2 md:gap-4 lg:max-w-[750px] xl:max-w-[920px] overflow-x-hidden">
           {carouselImages.map((image, index) => {
             const { src } = image;
             return (
               <div
-                className="md:min-w-[388px] md:min-h-[288px] lg:min-w-[488px] lg:min-h-[388px] relative inner rounded-xl overflow-hidden"
+                className="min-w-[288px] min-h-[188px] lg:min-w-[388px] lg:min-h-[288px] xl:min-w-[488px] xl:min-h-[388px] relative inner rounded-xl overflow-hidden"
                 key={index}
                 style={{
                   transform: `translate(-${currentGameIndex * (488 + 16)}px)`,
@@ -50,7 +50,7 @@ const CarouselPage = ({ galactus, montserrat, righteous }) => {
             );
           })}
         </div>
-        <div className="flex gap-5">
+        <div className="flex gap-5 justify-center">
           <button
             onClick={() =>
               setCurrentGameIndex((curr) => {
